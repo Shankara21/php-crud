45de8b01f2d5543f76c89ca5294c29becaf4ddf8
Shankara21 <lazuardit21@gmail.com>
2022-10-27 16:21:43 +0700

    update

--- index.php
+++ index.php
@@ -5,11 +5,16 @@ include 'functions.php';
 // Home Page template below.
 ?>
 
-<?=template_header('Home')?>
+<?= template_header('Home') ?>
 
 <div class="content">
 	<h2>Home</h2>
 	<p>Welcome to the home page!</p>
+	<hr>
+	<p>UTS Komputasi Awan</p>
+	<p>Muhammad Lazuardi Timur</p>
+	<p>TI 3E - 21</p>
+	<p>2041720114</p>
 </div>
 
-<?=template_footer()?>
+<?= template_footer() ?>
